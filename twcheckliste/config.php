44bcd77519c6405e75ff7052ffe4ad6cb36de24c
Torsten Wolter <web@agentur-triebwerk.de>
2016-01-14 09:55:53 +0100

    Anpassungen an aktuelle Dokuwiki Version

--- twcheckliste/config.php
+++ twcheckliste/config.php
@@ -1,6 +1,6 @@
 
 <?
-	$conf["logo"] = "/lib/plugins/twcheckliste/theme_twCheckliste/img/logo_triebwerk_slogan.png";
+	$conf["logo"] = "/lib/plugins/twcheckliste/theme_twCheckliste/img/logo_default.png";
 	$conf["farbcode"] = "#88BA15";
 	$conf["footertext"] = "Geprüft durch";
 ?>
